5a1b8e0c7db226d305665023662b68bdccd6c636
Sanidhaya Sharma <sanidhyas141@gmail.com>
2024-05-18 17:39:26 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,32 @@
 import 'package:flutter/material.dart';
+// import 'package:tictactoe/provider/room_data_provider.dart';
+import 'package:tictactoe/screens/create_room_screen.dart';
+// import 'package:tictactoe/screens/game_screen.dart';
+import 'package:tictactoe/screens/join_room_screen.dart';
+import 'package:tictactoe/screens/main_menu_screen.dart';
 import 'package:tictactoe/utils/colors.dart';
+// import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: bgColor),
-      home: const Text('Flutter Demo Home Page'),
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: bgColor,
+      ),
+      routes: {
+        MainMenuScreen.routeName: (context) => const MainMenuScreen(),
+        JoinRoomScreen.routeName: (context) => const JoinRoomScreen(),
+        CreateRoomScreen.routeName: (context) => const CreateRoomScreen(),
+      },
+      initialRoute: MainMenuScreen.routeName,
     );
   }
 }
